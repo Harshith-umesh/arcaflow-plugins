d5fb9b513a3694830e6684492bdfd6e9e3c173c8
Harshith-umesh <harshith.umesh.nat@gmail.com>
2022-09-09 16:06:55 +0530

    fix non default types

--- python/kube-burner/kubeburner_schema.py
+++ python/kube-burner/kubeburner_schema.py
@@ -26,7 +26,7 @@ class KubeBurnerCommonInputParams:
     """
     This is the data structure for the common input parameters for kube-burner workloads.
     """
-    waitFor: typing.List[str]
+    waitFor: typing.List[str] = field(default=[],metadata={"name": "Wait for ", "desscription":"Wait for the resources of this list to be ready"})
     writeToFile: bool = field(default="false",metadata={"name": "Write to file", "desscription": "Whether to dump collected metrics to files locally"})
     indexing: bool = field(default="true",metadata={"name": "INDEXING", "description": "Enable/disable indexing"})
     es_server: str = field(default="https://search-perfscale-dev-chmf5l4sh66lvxbnadi4bznl3a.us-west-2.es.amazonaws.com:443",metadata={"name": "Elasticsearch server url", "description": "URL for your elasticsearch endpoint"})
@@ -48,7 +48,7 @@ class KubeBurnerCommonInputParams:
 @dataclass
 class KubeBurnerPodDensityInputParams(KubeBurnerCommonInputParams):
     """
-    This is the data structure for the input parameters for kube-burner pod density workload.
+    This is the data structure for the input parameters for kube-burner pod-density workload.
     """
 
     podReadyThreshold: str = field(default="5000ms",metadata={"name": "POD_READY_THRESHOLD", "description": "Pod ready latency threshold (only applies to node-density and pod-density workloads)."})
@@ -60,17 +60,16 @@ class KubeBurnerPodDensityInputParams(KubeBurnerCommonInputParams):
 @dataclass
 class KubeBurnerClusterDensityInputParams(KubeBurnerCommonInputParams):
     """
-    This is the data structure for the input parameters for kube-burner cluster density workload.
+    This is the data structure for the input parameters for kube-burner cluster-density workload.
     """
 
     podNodeSelector: str = field(default="{node-role.kubernetes.io/worker: }",metadata={"name": "POD_NODE_SELECTOR", "description": "nodeSelector for pods created by the kube-burner workloads"})
     jobIterations: int = field(default="1000",metadata={"name": "TEST_JOB_ITERATIONS", "description": "This variable configures the number of pod-density jobs iterations to perform"})
 
-
 @dataclass
-class KubeBurnerNodeDensityInputParams(KubeBurnerCommonInputParams):
+class KubeBurnerCommonNodeDensityInputParams():
     """
-    This is the data structure for the input parameters for kube-burner cluster density workload.
+    This is the data structure for common input parameters of kube-burner node-density* workload.
     """
 
     NODE_COUNT: int = field(metadata={"name": "NODE_COUNT", "description": "Number of worker nodes to deploy the pods on"})
@@ -78,29 +77,39 @@ class KubeBurnerNodeDensityInputParams(KubeBurnerCommonInputParams):
    
 
 @dataclass
-class KubeBurnerNodeDensityHeavyInputParams(KubeBurnerCommonInputParams):
+class KubeBurnerNodeDensityInputParams(KubeBurnerCommonNodeDensityInputParams, KubeBurnerCommonInputParams):
     """
-    This is the data structure for the input parameters for kube-burner cluster density workload.
+    This is the data structure for the input parameters for kube-burner node-density workload.
     """
 
-    NODE_COUNT: int = field(metadata={"name": "NODE_COUNT", "description": "Number of worker nodes to deploy the pods on"})
-    PODS_PER_NODE: int = field(default="245",metadata={"name": "PODS_PER_NODE", "description": "the maximum number of pods to deploy on each labeled node."})
+    #NODE_COUNT: int = field(metadata={"name": "NODE_COUNT", "description": "Number of worker nodes to deploy the pods on"})
+    #PODS_PER_NODE: int = field(default="245",metadata={"name": "PODS_PER_NODE", "description": "the maximum number of pods to deploy on each labeled node."})
    
 
 @dataclass
-class KubeBurnerNodeDensityCniInputParams(KubeBurnerCommonInputParams):
+class KubeBurnerNodeDensityHeavyInputParams(KubeBurnerCommonNodeDensityInputParams, KubeBurnerCommonInputParams):
     """
-    This is the data structure for the input parameters for kube-burner cluster density workload.
+    This is the data structure for the input parameters for kube-burner node-density-heavy density workload.
     """
 
-    NODE_COUNT: int = field(metadata={"name": "NODE_COUNT", "description": "Number of worker nodes to deploy the pods on"})
-    PODS_PER_NODE: int = field(default="245",metadata={"name": "PODS_PER_NODE", "description": "the maximum number of pods to deploy on each labeled node."})
+    #NODE_COUNT: int = field(metadata={"name": "NODE_COUNT", "description": "Number of worker nodes to deploy the pods on"})
+    #PODS_PER_NODE: int = field(default="245",metadata={"name": "PODS_PER_NODE", "description": "the maximum number of pods to deploy on each labeled node."})
+   
+
+@dataclass
+class KubeBurnerNodeDensityCniInputParams(KubeBurnerCommonNodeDensityInputParams, KubeBurnerCommonInputParams):
+    """
+    This is the data structure for the input parameters for kube-burner node-density-cni density workload.
+    """
+
+    #NODE_COUNT: int = field(metadata={"name": "NODE_COUNT", "description": "Number of worker nodes to deploy the pods on"})
+    #PODS_PER_NODE: int = field(default="245",metadata={"name": "PODS_PER_NODE", "description": "the maximum number of pods to deploy on each labeled node."})
    
 
 @dataclass
 class KubeBurnerOutput:
     """
-    This is the data structure for output returned by the kube-burner indexer.
+    This is the data structure for output returned by kube-burner workloads.
     """
     uuid: str = field(metadata={"name": "UUID", "description": "UUID generated for this workload run"})
     output: str = field(metadata={"name": "Kube burner workload output", "description": "Output generated by the kube burner workload"})
